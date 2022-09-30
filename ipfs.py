65e781edab614d2dcbd5f3500e625292b29f6a0b
Taoli93 <48353960+LEEJSLT@users.noreply.github.com>
2022-10-01 00:00:04 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -32,9 +32,9 @@ def get_from_ipfs(cid,content_type="json"):
 	IPFS_API_endpoint = 'https://ipfs.infura.io:5001'
 	project_secret = '2066e379d1f4119e39665ceaadc0eabb'
 
-	params = (
-		('arg',cid)
-		)
+	params = {
+		'arg':cid
+	}
 	response = requests.post('https://ipfs.dev.infura.org:5001/api/v0/pin/rm',params=params, auth=(project_id, project_secret))
 	print(response.json())
 
